b533de9f4687fd02baea749f310e1168053c3b88
rvcoutinho <me@rvcoutinho.com>
2026-04-27 08:01:09 -0300

    Bumping messaging-service to 2.35.0;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/test/TestHelper.java
+++ src/main/java/org/coldis/library/test/TestHelper.java
@@ -273,7 +273,7 @@ public class TestHelper {
 	 */
 	@SuppressWarnings("resource")
 	public static GenericContainer<?> createArtemisContainer() {
-		return new GenericContainer<>("coldis/infrastructure-messaging-service:2.34")
+		return new GenericContainer<>("coldis/infrastructure-messaging-service:2.35.0")
 				.withCreateContainerCmdModifier(
 						cmd -> cmd.getHostConfig().withCpuCount(TestHelper.getCpuQuota()).withMemoryReservation(TestHelper.getMemoryReservationQuota())
 								.withMemory(TestHelper.getMemoryQuota()).withDiskQuota(TestHelper.getDiskQuota()))
